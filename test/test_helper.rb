ccd66a7dfe203360d4072d9621b1e929752b55d3
Daniel P. Clark <6ftdan@gmail.com>
2018-08-02 21:26:13 -0400

    remove test helpers since no Gemfile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'rutie'
 require 'minitest/autorun'
-require 'minitest/reporters'
-require 'color_pound_spec_reporter'
-
-Minitest::Reporters.use! [ColorPoundSpecReporter.new]
